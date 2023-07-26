2fb4ff1bfc1c08f2b99201368b5bc25979d5764d
Ho <ws000617@gmail.com>
2023-07-26 09:50:34 +0200

    Check Ads, Update OLED

--- Check_ADS1015.py
+++ Check_ADS1015.py
@@ -1,12 +1,12 @@
 import board
 import busio
 
-# I2C 주소 검색 함수
+# ADS1015_Search function 
 def find_ads1015_address():
     i2c = busio.I2C(board.SCL, board.SDA)
     device_addresses = i2c.scan()
 
-    ads1015_addresses = [0x40, 0x41, 0x3C, 0x60]  # ADS1015의 가능한 I2C 주소 리스트
+    ads1015_addresses = [0x40, 0x41, 0x3C, 0x60]  # Possible ADS1015 Address list
 
     for address in device_addresses:
         if address in ads1015_addresses:
@@ -14,11 +14,11 @@ def find_ads1015_address():
 
     return None
 
-# ADS1015 주소 검색
+# SEARCH ADS1015 Address
 ads1015_address = find_ads1015_address()
 
 if ads1015_address is not None:
-    print(f"ADS1015가 검색되었습니다. I2C 주소: 0x{ads1015_address:02X}")
+    print(f"ADS1015 O_ I2C Address: 0x{ads1015_address:02X}")
 else:
-    print("ADS1015가 검색되지 않았습니다.")
+    print("ADS1015 X")
 
--- oled.py
+++ oled.py
@@ -1,34 +1,10 @@
 import time
 import board
-import busio
-import adafruit_ads1x15.ads1015 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
 import adafruit_ssd1306
 from PIL import Image, ImageDraw, ImageFont
 import socket
+from piracer.vehicles import PiRacerStandard, PiracerBase
 
-# OLED 설정
-WIDTH = 128
-HEIGHT = 32
-i2c = busio.I2C(board.SCL, board.SDA)
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
-
-# ADS1015 설정
-ads = ADS.ADS1015(i2c)
-chan = AnalogIn(ads, ADS.P0)
-
-# 전압 측정 함수
-def read_battery_voltage():
-    # ADC 값을 전압으로 변환
-    voltage = chan.voltage
-
-    # 전압 값을 0V ~ 최대 전압 범위로 스케일링
-    max_voltage = 5.0  # 배터리의 최대 전압 값으로 변경 (예: 3.7V 배터리인 경우 3.7)
-    scaled_voltage = (voltage / max_voltage) * 100.0
-
-    return scaled_voltage
-
-# IP 주소 가져오는 함수
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -40,35 +16,35 @@ def get_ip():
     finally:
         s.close()
     return IP
+    
+piracer = PiracerStandard() 
+ 
+# Set I2C address (0x3C is the default address for SSD1306-based OLED displays)
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3C)
 
-# OLED 초기화
+# get ip_address and battery level
+ip = get_ip()
+battery = piracer.get_battery_voltage()
+
+# Clear the display
 oled.fill(0)
 oled.show()
-width = oled.width
-height = oled.height
-image = Image.new("1", (width, height))
+
+# Create blank image for drawing
+image = Image.new("1", (oled.width, oled.height))
 draw = ImageDraw.Draw(image)
+
+# Load default font
 font = ImageFont.load_default()
 
-try:
-    while True:
-        # 배터리 전압 측정
-        battery_voltage = read_battery_voltage()
+# Display text
+draw.text((0, 0), ip, font=font, fill=255)
+draw.text((0, 0), str(round(battery, 1))+"%", font=font, fill=255)
 
-        # IP 주소 가져오기
-        ip_address = get_ip()
+oled.image(image)
+oled.show()
 
-        # OLED 디스플레이에 배터리 잔량과 IP 주소 표시
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        draw.text((0, 0), "Battery Level:", font=font, fill=255)
-        draw.text((0, 16), "{:.1f}%".format(battery_voltage), font=font, fill=255)
-        draw.text((0, 24), "IP: {}".format(ip_address), font=font, fill=255)
-        oled.image(image)
-        oled.show()
-
-        # 1초마다 반복
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    pass
+# Wait for a few seconds
+#time.sleep(5)
 
