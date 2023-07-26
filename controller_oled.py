9d71f1cabbf69b7d8dd406daf482dcbaa92dae0b
Ho <ws000617@gmail.com>
2023-07-26 14:48:06 +0200

    Controller + LED

--- controller_oled.py
+++ controller_oled.py
@@ -6,15 +6,14 @@ from PIL import Image, ImageDraw, ImageFont
 import socket
 from piracer.vehicles import PiRacerStandard
 from piracer.gamepads import ShanWanGamepad
-import threading
 
-# OLED 설정
+# Declared OLED
 WIDTH = 128
 HEIGHT = 32
 i2c = busio.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
-# IP 주소 가져오는 함수
+# Get IP Address
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
@@ -27,51 +26,54 @@ def get_ip():
         s.close()
     return IP
 
-# OLED에 IP 주소와 배터리 잔량 표시하는 함수
-def display_ip_and_battery():
-    ip = get_ip()
-    battery = piracer.get_battery_voltage()
-
+# Display Battery Level and IP Address in OLED
+def display_info(ip, battery):
+    # Clear the display
     oled.fill(0)
+    oled.show()
+
+    # Create blank image for drawing
     image = Image.new("1", (oled.width, oled.height))
     draw = ImageDraw.Draw(image)
 
+    # Load default font
     font = ImageFont.load_default()
 
+    # Display IP address and battery level
     draw.text((0, 0), ip, font=font, fill=255)
-    draw.text((0, 16), str(round(battery, 1)) + "%", font=font, fill=255)
-
+    draw.text((0, 10), f"Battery: {str(round(battery, 1))}%", font=font, fill=255)
+ 
     oled.image(image)
     oled.show()
 
-# 스레드로 OLED 정보를 주기적으로 업데이트
-def oled_thread():
-    while True:
-        display_ip_and_battery()
-        time.sleep(5.0)
+if __name__ == '__main__':
+    piracer = PiRacerStandard()
+    shanwan_gamepad = ShanWanGamepad()
 
-# 컨트롤러 조작 처리
-def controller_handler():
-    while True:
-        gamepad_input = shanwan_gamepad.read_data()
-        throttle = gamepad_input.analog_stick_left.y
-        steering = gamepad_input.analog_stick_right.y
+    try:
+        while True:
+            # Control Piracer using Controller
+            gamepad_input = shanwan_gamepad.read_data()
+            throttle = gamepad_input.analog_stick_left.y
+            steering = gamepad_input.analog_stick_right.y
 
-        print(f'throttle={throttle}, steering={steering}')
+            if abs(throttle) == 0 and abs(steering) == 0:
+                # Input X
+                ip = get_ip()
+                battery = piracer.get_battery_voltage()
+                display_info(ip, battery)
+            else:
+                # Input O
+                piracer.set_throttle_percent(throttle * 0.5)
+                piracer.set_steering_percent(steering)
+            
+            print(f'throttle: {throttle}, steering: {steering}')
+   
+            # Delay
+            #time.sleep(0.1)
 
-        piracer.set_throttle_percent(throttle * 0.5)
-        piracer.set_steering_percent(steering)
+    except KeyboardInterrupt:
+        # Ctrl+C 
+        piracer.set_throttle_percent(0)
+        piracer.set_steering_percent(0)
 
-# OLED 스레드와 컨트롤러 핸들러 스레드를 생성하고 실행
-oled_thread = threading.Thread(target=oled_thread)
-controller_thread = threading.Thread(target=controller_handler)
-
-oled_thread.start()
-controller_thread.start()
-
-# 메인 스레드가 종료되지 않도록 유지
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
