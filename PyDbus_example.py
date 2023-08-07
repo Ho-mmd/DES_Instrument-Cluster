2f8e41749d09908762b7069df52925d5a23c50f6
S-LEE <seungwoo991123@gmail.com>
2023-08-07 16:29:03 +0200

    Change the Data buffer

--- PyDbus_example.py
+++ PyDbus_example.py
@@ -29,7 +29,7 @@ class DbusService:
 
 bus = SessionBus()
 dbus_service = DbusService()
-bus.publish("com.example.CanData", dbus_service)
+bus.publish("com.example.canData", dbus_service)  # Use a different name here
 
 def receive_can_data():
     bus = can.interface.Bus(channel='can0', bustype='socketcan')
@@ -37,9 +37,19 @@ def receive_can_data():
         message = bus.recv()
         if message is not None:
             data = message.data
-            rpm, speed = struct.unpack('<ff', data[:4]), struct.unpack('<ff', data[4:])
-            dbus_service.current_rpm = rpm[0]
-            dbus_service.current_speed = speed[0]
+
+            # Unpack data as two 4-byte floats (RPM and Speed)
+            rpm, speed = struct.unpack('<ff', data)
+
+            # Convert RPM to 8-byte float and fill the remaining bytes with zeros
+            rpm_8bytes = struct.unpack('<Q', struct.pack('<d', rpm))[0] >> 32
+
+            # Convert Speed to 8-byte float and fill the remaining bytes with zeros
+            speed_8bytes = struct.unpack('<Q', struct.pack('<d', speed))[0]
+
+            # Update current_rpm and current_speed with the converted values
+            dbus_service.current_rpm = struct.unpack('<d', struct.pack('<Q', rpm_8bytes))[0]
+            dbus_service.current_speed = struct.unpack('<d', struct.pack('<Q', speed_8bytes))[0]
 
 GLib.idle_add(receive_can_data)
 
