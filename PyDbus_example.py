957cbe9ac3cbaacecb1170eb56f029309fc38a54
S-LEE <seungwoo991123@gmail.com>
2023-08-08 15:15:36 +0200

    new version

--- PyDbus_example.py
+++ PyDbus_example.py
@@ -1,37 +1,27 @@
 import can
-from pydbus import SessionBus
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
 import struct
 
-class DbusService:
-    dbus = """
-    <node>
-        <interface name='com.example.CanData'>
-            <method name='getCurrentSpeed'>
-                <arg type='d' name='speed' direction='out'/>
-            </method>
-            <method name='getCurrentRPM'>
-                <arg type='d' name='rpm' direction='out'/>
-            </method>
-        </interface>
-    </node>
-    """
+# Initialize D-Bus main loop
+DBusGMainLoop(set_as_default=True)
 
-    def __init__(self):
+class DbusSpeed(dbus.service.Object):
+    def __init__(self, bus_name, object_path):
+        dbus.service.Object.__init__(self, bus_name, object_path)
         self.current_speed = 0.0
         self.current_rpm = 0.0
 
-    def getCurrentSpeed(self):
+    @dbus.service.Property(dbus.Double)
+    def Get_CurrentSpeed(self):
         return self.current_speed
 
-    def getCurrentRPM(self):
+    @dbus.service.Property(dbus.Double)
+    def Get_CurrentRPM(self):
         return self.current_rpm
 
-bus = SessionBus()
-dbus_service = DbusService()
-bus.publish("com.example.canData", dbus_service)  # Use a different name here
-
-def receive_can_data():
+def receive_can_data(dbus_speed):
     bus = can.interface.Bus(channel='can0', bustype='socketcan')
     while True:
         message = bus.recv()
@@ -41,20 +31,26 @@ def receive_can_data():
             # Unpack data as two 4-byte floats (RPM and Speed)
             rpm, speed = struct.unpack('<ff', data)
 
-            # Convert RPM to 8-byte float and fill the remaining bytes with zeros
-            rpm_8bytes = struct.unpack('<Q', struct.pack('<d', rpm))[0] >> 32
+            # Convert RPM and Speed to 8-byte long long unsigned int
+            rpm_8bytes = struct.unpack('<Q', struct.pack('<f', rpm))[0]
+            speed_8bytes = struct.unpack('<Q', struct.pack('<f', speed))[0]
 
-            # Convert Speed to 8-byte float and fill the remaining bytes with zeros
-            speed_8bytes = struct.unpack('<Q', struct.pack('<d', speed))[0]
+            # Perform right shift by 32 bits
+            rpm_4bytes = rpm_8bytes >> 32
+            speed_4bytes = speed_8bytes >> 32
 
-            # Update current_rpm and current_speed with the converted values
-            dbus_service.current_rpm = struct.unpack('<d', struct.pack('<Q', rpm_8bytes))[0]
-            dbus_service.current_speed = struct.unpack('<d', struct.pack('<Q', speed_8bytes))[0]
+            # Convert back to double and allocate data in current_rpm && speed
+            dbus_speed.current_rpm = struct.unpack('<d', struct.pack('<Q', rpm_4bytes))[0]
+            dbus_speed.current_speed = struct.unpack('<d', struct.pack('<Q', speed_4bytes))[0]
 
+            print(f"Received RPM: {dbus_speed.CurrentRPM}, Speed: {dbus_speed.CurrentSpeed}")
 
-            print(f"Received RPM: {dbus_service.current_rpm}, Speed: {dbus_service.current_speed}")
+# Start D-Bus service
+bus_name = dbus.service.BusName("com.example.CanData", dbus.SessionBus())
+dbus_speed = DbusSpeed(bus_name, "/com/example/CanData/Speed")
 
-GLib.idle_add(receive_can_data)
+# Start receiving CAN data and publishing on D-Bus
+GLib.idle_add(receive_can_data, dbus_speed)
 
 loop = GLib.MainLoop()
 loop.run()
