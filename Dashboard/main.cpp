0444e496c99c814d048569f9a83491fa74c59786
ho <ws000617@gmail.com>
2023-08-11 07:45:16 +0200

    Add comment

--- Dashboard/main.cpp
+++ Dashboard/main.cpp
@@ -1,47 +1,64 @@
+// Necessary Qt includes for our application
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include "main.h"
 
+// CanReceiver class constructor definition
 CanReceiver::CanReceiver() {
-    // DBus 서비스 등록
+    // Register the D-Bus service and the D-Bus object for our application
     QDBusConnection::sessionBus().registerService("com.example.CanData");
     QDBusConnection::sessionBus().registerObject("/com/example/CanData/Speed", this, QDBusConnection::ExportAllSlots);
 }
 
-float CanReceiver::speed() const {
+// Getter function for speed
+double CanReceiver::speed() const {
     return m_speed;
 }
 
-float CanReceiver::rpm() const {
+// Getter function for rpm
+double CanReceiver::rpm() const {
     return m_rpm;
 }
 
+// Method to set the speed and rpm data, and emit signals when they change
 void CanReceiver::setData(double speed, double rpm) {
     m_speed = speed;
     m_rpm = rpm;
-    emit speedChanged();
-    emit rpmChanged();
+    emit speedChanged(); // Notify any listeners that speed has changed
+    emit rpmChanged();   // Notify any listeners that rpm has changed
 }
 
+// Main function for the application
 int main(int argc, char *argv[])
 {
+    // Enable High DPI scaling for devices that support it
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
+    // Create a QGuiApplication object for our GUI application
     QGuiApplication app(argc, argv);
 
+    // Create a QQmlApplicationEngine object for our QML engine
     QQmlApplicationEngine engine;
 
+    // Instantiate our CanReceiver class
     CanReceiver canReceiver;
+    // Expose our CanReceiver instance to QML as "canReceiver"
     engine.rootContext()->setContextProperty("canReceiver", &canReceiver);
 
+    // Load our main QML file
     const QUrl url(QStringLiteral("qrc:/main.qml"));
+    // Connect a signal to handle if object creation fails in the QML engine
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
+        // If object creation fails, exit the application
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
+
+    // Load the QML file into our engine
     engine.load(url);
 
+    // Run our application's event loop
     return app.exec();
 }
--- Dashboard/main.h
+++ Dashboard/main.h
@@ -1,31 +1,46 @@
+// Prevents the header from being included multiple times in a single compilation.
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <QObject>
-#include <QDBusConnection>
+// Necessary includes for the class.
+#include <QObject>           // Base class for objects in Qt's object system.
+#include <QDBusConnection>   // Provides a connection to the D-Bus messaging system.
 
+// Declare a class named CanReceiver that inherits from QObject.
 class CanReceiver : public QObject
 {
+    // Qt's macro to enable meta-object features like signals, slots, and properties.
     Q_OBJECT
-    Q_PROPERTY(float speed READ speed NOTIFY speedChanged)
-    Q_PROPERTY(float rpm READ rpm NOTIFY rpmChanged)
+
+    // Property declarations for the class.
+    // They can be accessed from QML or any Qt meta-object system.
+    // It has a READ accessor and a signal to notify when its value changes.
+    Q_PROPERTY(float speed READ speed NOTIFY speedChanged)   // Property for speed.
+    Q_PROPERTY(float rpm READ rpm NOTIFY rpmChanged)         // Property for rpm.
 
 public:
+    // Constructor declaration.
     CanReceiver();
 
-    float speed() const;
-    float rpm() const;
+    // Accessor methods (getters) for the properties.
+    double speed() const;    // Getter for the 'speed' property.
+    double rpm() const;      // Getter for the 'rpm' property.
 
 public slots:
+    // Slot declaration. Slots are methods that can be invoked in response to signals.
+    // In the context of D-Bus, this can also be a method that can be invoked over the bus.
     void setData(double speed, double rpm);
 
 signals:
-    void speedChanged();
-    void rpmChanged();
+    // Signal declarations. Signals are emitted to notify other parts of the application of certain events.
+    void speedChanged();    // Signal emitted when the 'speed' property changes.
+    void rpmChanged();      // Signal emitted when the 'rpm' property changes.
 
 private:
-    double m_speed;
-    double m_rpm;
+    // Private member variables to store the actual data.
+    double m_speed;         // Holds the speed data.
+    double m_rpm;           // Holds the rpm data.
 };
 
+// Ends the header guard.
 #endif // MAIN_H
